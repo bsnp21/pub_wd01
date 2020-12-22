7d3467611b1c4057e0cdc37cb8325da42db06fc0
root <root@ip-172-31-59-85.ec2.internal>
2020-12-22 01:07:44 +0000

    svr mac checkin

--- account/myoj/myNote_json.js
+++ account/myoj/myNote_json.js
@@ -5,7 +5,7 @@ var _bnotes=
             "1": "\n<ol>\n\t<li>@test</li>\n</ol>\n",
             "2": "\n<ol>\n\t<li>--</li>\n\t<li>formless&nbsp;</li>\n\t<li>invisible</li>\n</ol>\n",
             "3": "\n<ol>\n\t<li>1) Proverbs 8:30.&nbsp;</li>\n\t<li>we plural&nbsp;</li>\n\t<li>&nbsp;light is Jesus&nbsp;</li>\n\t<li>&nbsp;spirit over water 1:2 and mat14:25</li>\n\t<li>uu</li>\n</ol>\n",
-            "4": "",
+            "4": "<ol><li>--</li></ol>",
             "5": "",
             "6": "",
             "7": "",
