f096980ab376814d44549c5f13a5d30de4d200fb
root <root@ip-172-31-59-85.ec2.internal>
2020-12-27 13:03:15 +0000

    svr update _myNote~Gen32:25

--- account/myoj/myNote_json.js
+++ account/myoj/myNote_json.js
@@ -1017,7 +1017,7 @@ var _bnotes=
             "22": "",
             "23": "",
             "24": "<ol><li>Wrestle with God. Do not wrestle with evil.&nbsp;</li></ol>",
-            "25": "<ol><li>To wrestle with God, God let you win.&nbsp;</li><li>To wrestle with evil, Evil let you go to hell.&nbsp;</li></ol>",
+            "25": "<ol><li>To wrestle with God, God let you win. (Mat7:7,Mat7:8,Luk11:10,Luk11:9)</li><li>To wrestle with evil, Evil let you go to hell.&nbsp;</li></ol>",
             "26": "",
             "27": "",
             "28": "",
