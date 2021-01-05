828ed89227e106de342ecf0f49d4e1d3a4a09ea4
System Administrator <wdingbox@gmail.com>
2021-01-05 10:04:42 -0500

    svr update _myNote~Gen1:2.

--- account/myoj/myNote_json.js
+++ account/myoj/myNote_json.js
@@ -3,7 +3,7 @@ var _bnotes=
     "Gen": {
         "1": {
             "1": "\n<ol>\n\t<li>I am alpha and omega.&nbsp;</li><li>mm</li>\n</ol>\n",
-            "2": "\n<ol>\n\t<li>Water analog to trinity.&nbsp;</li>\n\t<li>formless mat1:1</li>\n\t<li>invisible</li><li>Jesus walked on waters.&nbsp;(<a href=\"#Mat14:26\">Mat14:26</a>)&nbsp;(<a href=\"#Mak6:48\">Mak6:48</a>)&nbsp;(<a href=\"#Jhn6:19\">Jhn6:19</a>)&nbsp;</li><li>Triune: God, Spirit and Jesus.&nbsp;</li>\n</ol>\n",
+            "2": "\n<ol>\n\t<li>Water analog to trinity.&nbsp;</li>\n\t<li>formless Jhn1:1</li>\n\t<li>invisible</li><li>Jesus walked on waters.&nbsp;(Mat14:26)&nbsp;(Mak6:48)&nbsp;(Jhn6:19)&nbsp;</li><li>Triune: God, Spirit and Jesus.&nbsp;</li>\n</ol>\n",
             "3": "\n<ol>\n\t<li>in Pro8:30. God said, Jesus finished. Spirit is light.&nbsp;</li>\n\t<li>we plural&nbsp;</li>\n\t<li>&nbsp;light is Jesus&nbsp;</li>\n\t<li>&nbsp;spirit over water Gen1:2 and Mat14:25</li>\n\t<li>uu</li>\n</ol>\n",
             "4": "<ol><li>--</li></ol>",
             "5": "",
