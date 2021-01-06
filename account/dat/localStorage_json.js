ff1e781ee79be78eb5e82075905c3a04a55c2d0a
System Administrator <wdingbox@gmail.com>
2021-01-06 16:06:59 -0500

    svr update ../../../../bible_study_notes/usrs/wdingbox/biblestudynote_pub_wd/account/dat/localStorage_json.js saved..

--- account/dat/localStorage_json.js
+++ account/dat/localStorage_json.js
@@ -1,5 +1,10 @@
 var localStorage = {
+    "repositories": "[{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_wd.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_test.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"}]",
+    "myNote": "",
+    "MostRecentSearchStrn": "[\"Sarah\"]",
     "FontSize": "16",
-    "repositories": "[{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_wd.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_test.git\",\"passcode\":\"\"}]",
-    "test": "1,2"
+    "#MemoryVerse": "[\"Col1:15\",\"Pro16:9\"]",
+    "test": "1,2",
+    "#RecentMarks": "[\"Col1:15\",\"Pro16:9\"]",
+    "#RecentBooks": "[\"Pro1:1\",\"Col1:1\"]"
 }
