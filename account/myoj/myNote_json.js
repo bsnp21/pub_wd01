ff4087111f017d6bc8f3d63e462e2ad2ce9f2327
System Administrator <wdingbox@gmail.com>
2021-01-06 13:24:31 -0500

    svr update _myNote~Col1:15 saved@ ../../../../bible_study_notes/usrs/wdingbox/biblestudynote_pub_wd/account/myoj/myNote_json.js.

--- account/myoj/myNote_json.js
+++ account/myoj/myNote_json.js
@@ -31794,7 +31794,7 @@ var _bnotes=
             "12": "",
             "13": "",
             "14": "",
-            "15": "<ol><li><a href=\"https://www.youtube.com/watch?v=VIxrGB6YIUQ&t=0m7s\">utube</a></li></ol>",
+            "15": "<ol><li><a href=\"https://www.youtube.com/watch?v=VIxrGB6YIUQ&t=0m7s\">utube</a> <a href=\"http://www.youtube.com/watch?v=VIxrGB6YIUQ&start=0m7s&end=0m15s\">youtube</a></li></ol>",
             "16": "",
             "17": "",
             "18": "",
