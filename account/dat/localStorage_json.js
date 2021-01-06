f9cab96eec84466b83213c1b6c3cb6953cb1680d
System Administrator <wdingbox@gmail.com>
2021-01-06 11:39:09 -0500

    svr update ../../../../bible_study_notes/usrs/wdingbox/biblestudynote_pub_wd/account/dat/localStorage_json.js saved..

--- account/dat/localStorage_json.js
+++ account/dat/localStorage_json.js
@@ -1,8 +1,9 @@
 var localStorage = {
     "FontSize": "16",
-    "repositories": "[{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_test.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_wd.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_testxx.git\",\"passcode\":\"3edcFDSAx\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_testxy.git\",\"passcode\":\"3edcFDSAx\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_testx.git\",\"passcode\":\"3edcFDSAx\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_testxxxx.git\",\"passcode\":\"3edcFDSAx\",\"repodesc\":\"\"}]",
-    "#MemoryVerse": "[\"Pro8:22\",\"Pro16:9\"]",
+    "repositories": "[{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_wd.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_test.git\",\"passcode\":\"\",\"repodesc\":\"\"}]",
+    "#MemoryVerse": "[\"Pro16:9\",\"Pro8:22\"]",
     "test": "1,2",
-    "#RecentMarks": "[\"Pro8:22\",\"Pro16:9\"]",
-    "#RecentBooks": "[\"Pro1:1\"]"
+    "myNote": "{\"host\":\"localhost:7778\",\"url\":\"http://localhost:7778/\",\"api\":\"ApiBibleObj_write_Usr_BkcChpVrs_txt\",\"inp\":{\"usr\":{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_wd.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"},\"par\":{\"fnames\":[\"_myNote\"],\"inpObj\":{\"Gen\":{\"24\":{\"29\":\"<ol><li>Laban contrast Abraham Gen14:23</li></ol>\"}}}}},\"m_src\":\"http://localhost:7778/ApiBibleObj_load_by_bibOj?inp=%7B%22usr%22%3A%7B%22repopath%22%3A%22https%3A%2F%2Fgithub.com%2Fwdingbox%2Fbiblestudynote_pub_wd.git%22%2C%22passcode%22%3A%223edcFDSA%22%2C%22repodesc%22%3A%22%22%7D%2C%22par%22%3A%7B%22fnames%22%3A%5B%22NIV%22%2C%22_myNote%22%5D%2C%22bibOj%22%3A%7B%22Gen%22%3A%7B%2224%22%3A%7B%7D%7D%7D%2C%22Search%22%3Anull%7D%7D\"}",
+    "#RecentMarks": "[\"Gen24:29\",\"Gen24:28\"]",
+    "#RecentBooks": "[\"Gen1:1\"]"
 }
