2e1012489149c65036fa36f6f63c4dd912879650
System Administrator <wdingbox@gmail.com>
2021-01-07 16:56:07 -0500

    svr update ../../../../bible_study_notes/usrs/wdingbox/biblestudynote_pub_wd/account/dat/localStorage_json.js saved..

--- account/dat/localStorage_json.js
+++ account/dat/localStorage_json.js
@@ -3,7 +3,7 @@ var localStorage = {
     "myNote": "",
     "MostRecentSearchStrn": "[\"Sarah\"]",
     "FontSize": "16",
-    "#MemoryVerse": "[\"1Ts2:2\",\"Phl1:30\",\"Eph6:12\",\"2Co10:3\",\"Eph2:2\",\"1Ts4:17\",\"1Co9:26\",\"Pro8:22\",\"Col1:15\",\"Pro16:9\"]",
+    "#MemoryVerse": "[\"Pro16:9\",\"Col1:15\",\"Pro8:22\",\"1Co9:26\",\"1Ts4:17\",\"Eph2:2\",\"2Co10:3\",\"Eph6:12\",\"Phl1:30\",\"1Ts2:2\"]",
     "test": "1,2",
     "#RecentMarks": "[\"Col1:15\",\"Pro16:9\"]",
     "#RecentBooks": "[\"1Pe1:1\",\"Pro1:1\",\"Col1:1\"]"
