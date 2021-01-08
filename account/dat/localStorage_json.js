4c07d02777ee653e24abbe0bff379c9a11b55ea9
root <root@ip-172-31-59-85.ec2.internal>
2021-01-08 23:34:42 +0000

    svr update ../../../../bible_study_notes/usrs/wdingbox/biblestudynote_pub_wd/account/dat/localStorage_json.js saved..

--- account/dat/localStorage_json.js
+++ account/dat/localStorage_json.js
@@ -1,10 +1,12 @@
 var localStorage = {
-    "repositories": "[{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_wd.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_wdxxx.git\",\"passcode\":\"3edcFDSAx\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_wd.gitx\",\"passcode\":\"3edcFDSAx\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_test.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"}]",
-    "myNote": "",
-    "MostRecentSearchStrn": "[\"Sarah\"]",
-    "FontSize": "16",
-    "#MemoryVerse": "[\"1Ts2:2\",\"Phl1:30\",\"Eph6:12\",\"2Co10:3\",\"Eph2:2\",\"1Ts4:17\",\"1Co9:26\",\"Pro8:22\",\"Col1:15\",\"Pro16:9\"]",
+    "#RecentMarks": "[\"1Ts4:17\",\"1Ts2:2\",\"Col1:15\",\"Col1:14\",\"Col1:1\",\"Phl1:30\",\"Eph6:12\",\"Eph2:2\",\"2Co10:3\",\"1Co9:26\",\"Jhn4:7\",\"Pro16:9\",\"Pro8:22\",\"Pro1:1\",\"Gen24:17\",\"Gen1:1\"]",
+    "MostRecentSearchFile": "NIV",
+    "repositories": "[{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_wd.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_test.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_testx.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"}]",
+    "FontSize": "20",
+    "myNote": "{\"host\":\"52.87.154.78:7778\",\"url\":\"http://52.87.154.78:7778/\",\"api\":\"ApiBibleObj_write_Usr_BkcChpVrs_txt\",\"inp\":{\"usr\":{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_wd.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"},\"par\":{\"fnames\":[\"_myNote\"],\"inpObj\":{\"Gen\":{\"24\":{\"17\":\"<ol><li>\\\"有一个撒玛利亚的妇人来打水．耶稣对他说、请你给我水喝。\\\" (Jhn4:7 CUVS)</li></ol>\"}}}}},\"m_src\":\"http://52.87.154.78:7778/ApiBibleObj_read_Usr_BkcChpVrs_txt?inp=%7B%22usr%22%3A%7B%22repopath%22%3A%22https%3A%2F%2Fgithub.com%2Fwdingbox%2Fbiblestudynote_pub_wd.git%22%2C%22passcode%22%3A%223edcFDSA%22%2C%22repodesc%22%3A%22%22%7D%2C%22par%22%3A%7B%22fnames%22%3A%5B%22_myNote%22%5D%2C%22inpObj%22%3A%7B%22Gen%22%3A%7B%2224%22%3A%7B%2217%22%3A%22%22%7D%7D%7D%7D%7D\"}",
+    "MostRecentSearchStrn": "[\"dragon\",\"tu zi\",\"zheng zhan\",\"kong zhong\",\"kong qi\",\"dou\"]",
+    "#RecentBooks": "[\"Gen1:1\",\"Col1:1\"]",
     "test": "1,2",
-    "#RecentMarks": "[\"Col1:15\",\"Pro16:9\"]",
-    "#RecentBooks": "[\"1Pe1:1\",\"Pro1:1\",\"Col1:1\"]"
+    "SelectedDocsList": "CUVS,CUVpy,NIV,ESV,_myNote",
+    "#MemoryVerse": "[\"Pro16:9\",\"Col1:15\",\"Pro8:22\",\"1Co9:26\",\"1Ts4:17\",\"Eph2:2\",\"2Co10:3\",\"Eph6:12\",\"Phl1:30\",\"1Ts2:2\"]"
 }
