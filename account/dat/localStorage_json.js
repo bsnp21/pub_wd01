f13a655a9760c7eaef5d6fbbb0b3c281d5dbaee9
root <root@ip-172-31-59-85.ec2.internal>
2021-01-10 01:32:55 +0000

    svr:../../../../bible_study_notes/usrs/bsnp21/pub_wd01/account/dat/localStorage_json.js saved.. repodesc:Yulin

--- account/dat/localStorage_json.js
+++ account/dat/localStorage_json.js
@@ -1,12 +1,12 @@
 var localStorage = {
-    "#RecentMarks": "[\"1Ts4:17\",\"1Ts2:2\",\"Col1:15\",\"Col1:14\",\"Col1:1\",\"Phl1:30\",\"Eph6:12\",\"Eph2:2\",\"2Co10:3\",\"1Co9:26\",\"Jhn4:7\",\"Pro16:9\",\"Pro8:22\",\"Pro1:1\",\"Gen24:17\",\"Gen1:1\"]",
     "MostRecentSearchFile": "NIV",
-    "repositories": "[{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_wd.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_test.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_testx.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"}]",
-    "FontSize": "20",
-    "myNote": "{\"host\":\"52.87.154.78:7778\",\"url\":\"http://52.87.154.78:7778/\",\"api\":\"ApiBibleObj_write_Usr_BkcChpVrs_txt\",\"inp\":{\"usr\":{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_wd.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"},\"par\":{\"fnames\":[\"_myNote\"],\"inpObj\":{\"Gen\":{\"24\":{\"17\":\"<ol><li>\\\"有一个撒玛利亚的妇人来打水．耶稣对他说、请你给我水喝。\\\" (Jhn4:7 CUVS)</li></ol>\"}}}}},\"m_src\":\"http://52.87.154.78:7778/ApiBibleObj_read_Usr_BkcChpVrs_txt?inp=%7B%22usr%22%3A%7B%22repopath%22%3A%22https%3A%2F%2Fgithub.com%2Fwdingbox%2Fbiblestudynote_pub_wd.git%22%2C%22passcode%22%3A%223edcFDSA%22%2C%22repodesc%22%3A%22%22%7D%2C%22par%22%3A%7B%22fnames%22%3A%5B%22_myNote%22%5D%2C%22inpObj%22%3A%7B%22Gen%22%3A%7B%2224%22%3A%7B%2217%22%3A%22%22%7D%7D%7D%7D%7D\"}",
-    "MostRecentSearchStrn": "[\"dragon\",\"tu zi\",\"zheng zhan\",\"kong zhong\",\"kong qi\",\"dou\"]",
-    "#RecentBooks": "[\"Gen1:1\",\"Col1:1\"]",
+    "repositories": "[{\"repopath\":\"https://github.com/bsnp21/pub_wd01.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"Yulin\"},{\"repopath\":\"https://github.com/bsnp21/pub_test01.git\",\"passcode\":\"pub_test01\",\"repodesc\":\"Yulin\"},{\"repopath\":\"https://github.com/bsnp21/pub_wd_01.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"Yulin\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_wd.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/wdingbox/biblestudynote_pub_test.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"}]",
+    "myNote": "{\"host\":\"52.87.154.78:7778\",\"url\":\"http://52.87.154.78:7778/\",\"api\":\"ApiBibleObj_write_Usr_BkcChpVrs_txt\",\"inp\":{\"usr\":{\"repopath\":\"https://github.com/bsnp21/pub_wd01.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"Yulin\"},\"par\":{\"fnames\":[\"_myNote\"],\"inpObj\":{\"Gen\":{\"26\":{\"9\":\"<ol><li>a Abimelech is a title of king. Not name of person. </li></ol>\"}}}}},\"m_src\":\"http://52.87.154.78:7778/ApiBibleObj_read_Usr_BkcChpVrs_txt?inp=%7B%22usr%22%3A%7B%22repopath%22%3A%22https%3A%2F%2Fgithub.com%2Fbsnp21%2Fpub_wd01.git%22%2C%22passcode%22%3A%223edcFDSA%22%2C%22repodesc%22%3A%22Yulin%22%7D%2C%22par%22%3A%7B%22fnames%22%3A%5B%22_myNote%22%5D%2C%22inpObj%22%3A%7B%22Gen%22%3A%7B%2226%22%3A%7B%229%22%3A%22%22%7D%7D%7D%7D%7D\"}",
+    "MostRecentSearchStrn": "[\"Beersheba\",\"别是巴\",\"Stephen\",\"St\",\"S\",\"Stev\",\"Steven\"]",
+    "SelectedDocsList": "CUVS,NIV,KJV,ESV,H_G,_myNote,_myTakeaway",
+    "FontSize": "16",
+    "#MemoryVerse": "[\"1Ts2:2\",\"Phl1:30\",\"Eph6:12\",\"2Co10:3\",\"Eph2:2\",\"1Ts4:17\",\"1Co9:26\",\"Pro8:22\",\"Col1:15\",\"Pro16:9\",\"1Co8:12\",\"Col2:23\",\"1Ti4:1\",\"Rom3:31\",\"Psm8:6\"]",
     "test": "1,2",
-    "SelectedDocsList": "CUVS,CUVpy,NIV,ESV,_myNote",
-    "#MemoryVerse": "[\"Pro16:9\",\"Col1:15\",\"Pro8:22\",\"1Co9:26\",\"1Ts4:17\",\"Eph2:2\",\"2Co10:3\",\"Eph6:12\",\"Phl1:30\",\"1Ts2:2\"]"
+    "#RecentMarks": "[\"Gen26:9\",\"Gen25:23\",\"1Co8:12\",\"Col2:23\",\"1Ti4:1\",\"Rom3:31\",\"Act15:1\",\"Amo9:11\",\"Act15:16\",\"Act15:13\",\"Act15:10\",\"Psm8:6\",\"Act8:2\",\"Pro16:9\"]",
+    "#RecentBooks": "[\"Gen1:1\",\"1Co1:1\",\"Col1:1\",\"1Ti1:1\",\"Phl1:1\",\"Rom1:1\",\"Amo1:1\",\"Act1:1\",\"Psm1:1\"]"
 }
