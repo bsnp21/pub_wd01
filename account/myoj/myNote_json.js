e1c4ba82892c7659a06057b22230d12818a34100
root <root@ip-172-31-59-85.ec2.internal>
2021-01-18 03:51:48 +0000

    svr:_myNote~Mat7:13 save-ok.. repodesc:

--- account/myoj/myNote_json.js
+++ account/myoj/myNote_json.js
@@ -25278,7 +25278,7 @@ var _bnotes=
             "10": "",
             "11": "",
             "12": "",
-            "13": "",
+            "13": "<ol><li>a narrow gate refers to the purity and holiness. Not something can be applied into everywhere. </li></ol>",
             "14": "",
             "15": "",
             "16": "",
