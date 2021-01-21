f73e6466469dd5ac11f8233754c8cd9d371c06a8
root <root@ip-172-31-59-85.ec2.internal>
2021-01-21 05:05:27 +0000

    Sync:len:416,dlt:85. repodesc:

--- account/dat/localStorage_json.js
+++ account/dat/localStorage_json.js
@@ -1,6 +1,8 @@
 var localStorage = {
     "FontSize": "16",
     "repositories": "[{\"repopath\":\"https://github.com/bsnp21/pub_wd01.git\",\"passcode\":\"3edcFDSA\",\"repodesc\":\"\"},{\"repopath\":\"https://github.com/bsnp21/pub_test.git\",\"passcode\":\"pub_test\",\"repodesc\":\"\"}]",
-    "#MemoryVerse": "[\"Pro16:9\",\"Mic6:8\",\"Mic6:1\"]",
-    "test": "1,2"
+    "#MemoryVerse": "[\"Col1:15\",\"Mic6:1\",\"Mic6:8\",\"Pro16:9\"]",
+    "test": "1,2",
+    "#RecentMarks": "[\"Col1:15\"]",
+    "#RecentBooks": "[\"Col1:1\"]"
 }
