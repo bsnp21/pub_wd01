69903d001e07c55de0136b9f8d2cc120fb7aa944
wdingsoft <wdingbox@gmail.com>
2021-02-01 17:55:49 -0500

    Sync:_myNote~Gen28:22 saved.. repodesc:test01

--- account/myoj/myNote_json.js
+++ account/myoj/myNote_json.js
@@ -851,7 +851,7 @@ var _bnotes=
             "19": "",
             "20": "",
             "21": "",
-            "22": ""
+            "22": "<ol><li>Jacob's Conditional Faith</li></ol>"
         },
         "29": {
             "1": "",
