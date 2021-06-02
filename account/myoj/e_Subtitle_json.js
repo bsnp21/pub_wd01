9e42b05719c4feb6ee10d7acbb8e0624c7a0e4e5
root <root@ip-172-31-87-112.ec2.internal>
2021-06-02 02:23:01 +0000

    Sync:e_Subtitle~Luk4:1 saved.. repodesc:

--- account/myoj/e_Subtitle_json.js
+++ account/myoj/e_Subtitle_json.js
@@ -27489,7 +27489,7 @@ var _Max_struct={
             "38": ""
         },
         "4": {
-            "1": "",
+            "1": "<a class=\"e_Subtitle\">#Temptation</a>",
             "2": "",
             "3": "",
             "4": "",
