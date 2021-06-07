54e0e0f06253459e376e8b44635b742a6c3c3ffc
root <root@ip-172-31-87-112.ec2.internal>
2021-06-07 14:48:32 +0000

    Sync:e_Note~Jhn4:4 saved.. repodesc:

--- account/myoj/e_Note_json.js
+++ account/myoj/e_Note_json.js
@@ -28245,7 +28245,7 @@ var _bnotes=
             "1": "",
             "2": "",
             "3": "",
-            "4": "",
+            "4": "<ol><li>Nicodemus and Samaria comparison</li><li>Common: one to one. </li><li>Difference: names, knowledge, social status, gender, location, noble, topics, results.</li></ol>",
             "5": "",
             "6": "",
             "7": "",
