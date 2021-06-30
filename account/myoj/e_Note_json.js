4524180bbea1b85e83e85a330d05b1379fcfda0d
root <root@ip-172-31-87-112.ec2.internal>
2021-06-30 16:14:28 +0000

    Sync:e_Note~Rom6:11 saved.. repodesc:

--- account/myoj/e_Note_json.js
+++ account/myoj/e_Note_json.js
@@ -30269,7 +30269,7 @@ var _bnotes=
             "8": "<ol><li>Rom1:17 the righteous live by faith. </li></ol>",
             "9": "<ol><li>The power of gospel.</li><li>Resurrection is not mysterious but the power to overwhelm the death. </li></ol>",
             "10": "<ol><li>For us he did so. </li><li>Once for all time work — power of God—no experience </li></ol>",
-            "11": "<ol><li>How?</li></ol>",
+            "11": "<ol><li>How? The immigrants the journey soujgenor on earth. </li></ol>",
             "12": "<ol><li>Overcoming the body. Like the horse. </li></ol>",
             "13": "",
             "14": "<ol><li>Good news. After reborn once live forever. </li></ol>",
