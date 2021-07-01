c683b3b4d619e8be48c69b85bdccbe76a4a65330
root <root@ip-172-31-87-112.ec2.internal>
2021-07-01 01:42:32 +0000

    Sync:e_Note~Rom1:18 saved.. repodesc:

--- account/myoj/e_Note_json.js
+++ account/myoj/e_Note_json.js
@@ -30128,7 +30128,7 @@ var _bnotes=
             "15": "<ol><li>Preach the gospel as much as I can — missiological principle. </li></ol>",
             "16": "<ol><li>Why am I shamed of the gospel?  (1) odd (2) unpopular (3) difficult to understand (4) unrighteous, evil (5) sexual (6) ungodly (7) heresy, false (8) unreliable, unfaithful. (9) useless, unrelated, ineffective, weakness. </li><li>Any non-gospel or teaching is shameful. </li><li>I have no shame because of Gospel —return back to Adam and Eve before the fall — even naked. </li><li>Not ashamed but glorious. </li><li>I used to be a shamed introvert because of the sin nature. Gospel told me my innate by the truth. </li></ol>",
             "17": "<ol><li>Definition of righteousness. </li><li>Gospel — faith — righteousness—driven power/energy. </li><li>Chinese Menzi’s righteousness (flesh blood based) vs God’s righteousness (faith based)</li><li>People love to be called the righteousness </li><li>hub2:4, <a href=\"https://www.desiringgod.org/messages/the-just-shall-live-by-faith\">JonhPepper</a> (1) too late to do (2) inevitability of fall. </li><li>Jesus never told us He is righteous. The ‘righteous’ is a word used for judgment. We have no right to judge people. Jesus revealed his righteous through his word and human judgment. Rom3:4. And his 5 indivisible physical experience— baptized, tempted, crucified, died, resurrected. </li></ol>",
-            "18": "",
+            "18": "<ol><li>Wrath also in Jesus. </li></ol>",
             "19": "",
             "20": "",
             "21": "",
