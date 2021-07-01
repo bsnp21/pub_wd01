b47332e534a7306fcb652ba1460bd50384e6e727
root <root@ip-172-31-87-112.ec2.internal>
2021-07-01 00:59:03 +0000

    Sync:e_Note~Rom5:20 saved.. repodesc:

--- account/myoj/e_Note_json.js
+++ account/myoj/e_Note_json.js
@@ -30255,7 +30255,7 @@ var _bnotes=
             "17": "",
             "18": "<ol><li>Once only. The only one. Jesus. </li><li>To understand Jesus through sin-death-king. </li></ol>",
             "19": "",
-            "20": "",
+            "20": "<ol><li>Luk13:1 prodigal son</li></ol>",
             "21": ""
         },
         "6": {
