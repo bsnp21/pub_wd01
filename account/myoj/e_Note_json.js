0bb6282ceae4a541734389d6a818d1c93d9a673b
root <root@ip-172-31-87-112.ec2.internal>
2021-07-09 13:33:59 +0000

    Sync:e_Note~Rom4:2 saved.. repodesc:

--- account/myoj/e_Note_json.js
+++ account/myoj/e_Note_json.js
@@ -30210,7 +30210,7 @@ var _bnotes=
         },
         "4": {
             "1": "<ol><li>Abraham got nothing in flesh. </li><li>According to flesh: body, desire, emotions, beautiful wife, saving Lots action. </li><li>God’s selection vs God’s will, for him, no human logical reason. </li><li>No personal goal, target. No idealism. </li><li>Like in company but not like Chinese hun e - do nothing but done— Daoism is a atheism. </li></ol>",
-            "2": "<ol><li>Abraham actions: (1) saving Lots. </li></ol>",
+            "2": "<ol><li>Abraham actions: (1) saving Lots. — righteous?no. Results (2) wife loving? No. (3) the most desired ideal is to multiply children—finished?</li></ol>",
             "3": "",
             "4": "",
             "5": "",
