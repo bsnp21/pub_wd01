21e25001a1514f669619c9512472490bb4a39faa
root <root@ip-172-31-87-112.ec2.internal>
2021-07-20 13:36:22 +0000

    Sync:e_Note~Rom5:3 saved.. repodesc:

--- account/myoj/e_Note_json.js
+++ account/myoj/e_Note_json.js
@@ -30238,7 +30238,7 @@ var _bnotes=
         "5": {
             "1": "<ol><li>Faith-righteousness-peace with God. </li><li>Peace and God are indivisible. </li></ol>",
             "2": "",
-            "3": "<ol><li>Not remove sufferings but overcome it — the power of Gospel </li></ol>",
+            "3": "<ol><li>Not remove sufferings but overcome it — the power of Gospel </li><li>The purpose of suffering </li></ol>",
             "4": "",
             "5": "",
             "6": "",
